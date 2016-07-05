c768d5f6771137869337bf094796422f489bd794
yonilevy <yonilevy@gmail.com>
2016-07-05 23:03:44 +0300

    drop the tests

--- spec/hoon-assist-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import HoonAssistView from '../lib/hoon-assist-view';
-
-describe('HoonAssistView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
